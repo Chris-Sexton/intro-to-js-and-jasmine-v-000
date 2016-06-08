ab8c87c12c9099e48c1afb3f18dd6fa909d4832b
Chris-Sexton <champlain2013@gmail.com>
2016-06-08 14:21:00 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(){
+  return "I love mint chocolate chip"
+}
+
+function shouting(sentence){
+  return sentence.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
+
+function theTruth(){
+  return true;
+}
